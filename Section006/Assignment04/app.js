7f5e37a5469a5dc6b46d61aba416c9536202b0d3
alexei <alexei.jobs@gmail.com>
2024-04-07 14:37:26 +0300

    work prep to merge

--- Section006/Assignment04/app.js
+++ Section006/Assignment04/app.js
@@ -1,11 +1,14 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.use('/add-product', (req,res,next) => {
-    res.send('<h1>The "Add product" Page</h1>');
+app.use('/add', (req,res,next) => {
+    console.log('add-product');
+    res.send('<h1>The Add product Page</h1>');
 });
+
 app.use('/', (req,res,next) => {
+    console.log('hello-express');
     res.send('<h1>Hello from express!</h1>');
 });
 
