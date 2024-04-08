5c1ff566e439ac8365f011c9db0b63f9b24a0f15
alexei <alexei.jobs@gmail.com>
2024-04-08 18:24:17 +0300

    add pug

--- Section006/Assignment04/app.js
+++ Section006/Assignment04/app.js
@@ -4,15 +4,18 @@ const express = require("express");
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine','pug');
+app.set('views','views');  //default, optional
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
 
-const adminRoutes = require('./routes/admin');
+// const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin'); 
 const shopRoutes = require('./routes/shop');
 
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use(adminData.routes);
 
 app.use((req,res,next) => {
     // res.status(404).send('<h1>Page not found</h1>');
--- Section006/Assignment04/routes/shop.js
+++ Section006/Assignment04/routes/shop.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 router.get('/', (req,res,next) => {
     // res.send('<h1>Hello from express!</h1>');
-    res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    // res.sendFile(path.join(__dirname,'../','views','shop.html'));
+    res.render('shop');
 });
 
 module.exports = router;
