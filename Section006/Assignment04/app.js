87637b8c6b9d439976f902ea5a72d1e788dac8bf
alexei <alexei.jobs@gmail.com>
2024-04-11 17:52:33 +0300

    Pug templates

--- Section006/Assignment04/app.js
+++ Section006/Assignment04/app.js
@@ -19,7 +19,9 @@ app.use(adminData.routes);
 
 app.use((req,res,next) => {
     // res.status(404).send('<h1>Page not found</h1>');
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render(path.join(__dirname,'views','404'));
+    
 });
 
 
