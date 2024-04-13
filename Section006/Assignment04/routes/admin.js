c5eeba92cfc344b82bf55ba172a1bb15c77093d4
Alexei Kravchuk <alexei.jobs@gmail.com>
2024-04-13 21:48:34 +0300

    Assign-prep4 compl.

--- Section006/Assignment04/routes/admin.js
+++ Section006/Assignment04/routes/admin.js
@@ -8,7 +8,8 @@ const products = [];
 
 router.get('/add-product', (req,res,next) => {
     // res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button> </input> </form>');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    // res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.render(path.join(__dirname,'../','views','add-product'),{docTitle: 'Add Product'});
 });
 
 router.post('/product', (req,res,next) => {
