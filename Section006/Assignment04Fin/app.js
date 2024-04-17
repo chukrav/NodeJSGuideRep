540f235ad1a6e410e155a1fbfa365a1a3983d418
Alexei Kravchuk <alexei.jobs@gmail.com>
2024-04-17 20:49:04 +0300

    home

--- Section006/Assignment04Fin/app.js
+++ Section006/Assignment04Fin/app.js
@@ -6,3 +6,12 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('view engine','pug');
 app.set('views','views');  //default, optional
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+const adminData = require('./routes/admin'); 
+const shopRoutes = require('./routes/users');
+
+app.use(shopRoutes);
+app.use(adminData.routes);
