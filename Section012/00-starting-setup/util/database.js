c0b68e5fc43f8008e1a576ce2c0e93bb5c521d9f
Alexei Kravchuk <alexei.jobs@gmail.com>
2024-06-12 22:02:33 +0300

    Removesequalize, start

--- Section012/00-starting-setup/util/database.js
+++ Section012/00-starting-setup/util/database.js
@@ -2,12 +2,19 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const connectURL = 'mongodb+srv://node-complete:nodeStudent@mflix.ryh22.mongodb.net/?retryWrites=true&w=majority&appName=mflix';
-MongoClient.connect(connectURL)
+const mongoConnect = (callback) => {
+  const connectURL = 'mongodb+srv://node-complete:nodeStudent@mflix.ryh22.mongodb.net/?retryWrites=true&w=majority&appName=mflix';
+  MongoClient.connect(connectURL)
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+  
+  // const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+  //   dialect: 'mysql',
+  //   host: 'localhost'
+  // });
+};
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
+module.exports = mongoConnect;
