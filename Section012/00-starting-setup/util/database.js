e19b363b56a1a70cdcedf5cf2976406b0cfc73d4
Alexei Kravchuk <alexei.jobs@gmail.com>
2024-06-12 21:19:00 +0300

    S12 start

--- Section012/00-starting-setup/util/database.js
+++ Section012/00-starting-setup/util/database.js
@@ -1,4 +1,9 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectURL = 'mongodb+srv://node-complete:nodeStudent@mflix.ryh22.mongodb.net/?retryWrites=true&w=majority&appName=mflix';
+MongoClient.connect(connectURL)
 
 const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
   dialect: 'mysql',
