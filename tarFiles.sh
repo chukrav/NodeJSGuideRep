0e4a45c8084789765ffd49be8a7fdf7b82862255
alexei <alexei.jobs@gmail.com>
2024-07-29 15:26:21 +0300

    Use sh script to clean

---
+++ tarFiles.sh
@@ -0,0 +1,6 @@
+tar -czvf data.tar.gz MySQLDB.txt \
+	./Section018/01-setup-and-basic-validation/app.js	\
+	./Section018/01-setup-and-basic-validation/controllers/auth.js
+rm MySQLDB.txt  \
+	./Section018/01-setup-and-basic-validation/app.js  \
+	./Section018/01-setup-and-basic-validation/controllers/auth.js
